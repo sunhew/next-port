6f1b2338ad0dac567de32c14360005b0fb1be7c7
최선화 <161446039+sunhew@users.noreply.github.com>
2024-06-26 12:22:47 +0900

    끝

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,24 +1,61 @@
 import { portText } from "@/constant";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 
-export default function Port() {
-    return (
-        <section id="port">
-            <div class="port__inner">
-                <div class="port__title">portfolio <em>포폴 작업물</em></div>
-                <div class="port__wrap">
-                    {portText.map((port, key) => (
-                        <div class="port__item" key="key">
-                            <span class="num">{port.num}.</span>
-                            <Image src={port.img} alt={port.title} width={420} height={262} />
-                            <h3 class="title">{port.title}</h3>
-                            <p class="desc">{port.desc}</p>
-                            <a href={port.view} target="_blank" class="site">사이트 보기</a>
-                        </div>
-                    ))}
+gsap.registerPlugin(ScrollTrigger);
 
+export default function Port() {
+    const horizontalRef = useRef(null);
+    const sectionRef = useRef([]);
+
+    useEffect(() => {
+        const horizontal = horizontalRef.current;
+        const sections = sectionRef.current;
+
+        let scrollTween = gsap.to(sections, {
+            xPercent: -120 * (sections.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: horizontal,
+                start: "top 56px",
+                end: () => "+=" + horizontal.offsetWidth,
+                pin: true,
+                scrub: 1,
+                invalidateOnRefresh: true,
+                anticipatePin: 1,
+            },
+        });
+
+        return () => {
+            scrollTween.kill();
+        }
+    }, []);
+
+    return (
+        <section id="port" ref={horizontalRef}>
+            <div className="port__inner">
+                <div className="port__title">portfolio <em>포폴 작업물</em></div>
+                <div className="port__wrap">
+                    {portText.map((port, key) => (
+                        <article
+                            className={`port__item p${key + 1}`}
+                            key={key}
+                            ref={(el) => (sectionRef.current[key] = el)}
+                        >
+                            <span className="num">{port.num}.</span>
+                            <a href={port.code} target="_blank" className="img">
+                                <Image src={port.img} alt={port.name} width={420} height={262} />
+                            </a>
+                            <h3 className="title">{port.title}</h3>
+                            <p className="desc">{port.desc}</p>
+                            <a href={port.view} target="_blank" className="site">사이트 보기</a>
+                        </article>
+                    ))}
                 </div>
             </div>
-        </section >
-    )
+        </section>
+    );
 }
